4d2b91c85af98e21d50512030427bcecdea3f804
Пользователь <zorina.lg2@skyeng.ru>
2023-03-14 22:53:56 +0300

    'new_varrrrrrr'

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import requests
 import urllib
 
-
+var12 = 'varrrr'
 vin = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 pole = list(range(10))
 pk = [4, 0, 2, 6, 8, 1, 3, 5, 7]
